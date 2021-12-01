847a02d2621ff4bcc9649f1552857be0ce66be2f
IlMike22 <michaelwidlok@gmail.com>
2021-12-01 16:44:52 +0100

    added AddEditNotesScreen and app navigation

--- app/src/main/java/com/mind/market/notes/feature_note/domain/use_case/GetNoteByIdUseCase.kt
+++ app/src/main/java/com/mind/market/notes/feature_note/domain/use_case/GetNoteByIdUseCase.kt
@@ -1,11 +1,12 @@
 package com.mind.market.notes.feature_note.domain.use_case
 
+import com.mind.market.notes.feature_note.domain.model.Note
 import com.mind.market.notes.feature_note.domain.repository.INoteRepository
 
 class GetNoteByIdUseCase(
-    private val repository:INoteRepository
+    private val repository: INoteRepository
 ) {
-    suspend operator fun invoke(noteId:Int) {
-        repository.getNoteById(noteId)
+    suspend operator fun invoke(noteId: Int): Note? {
+        return repository.getNoteById(noteId)
     }
 }
